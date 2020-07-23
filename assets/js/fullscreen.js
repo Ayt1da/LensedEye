a3332c4faa4b85bf3e1c6b7da2277550b5317295
Ayt1da <Ayt1da@users.noreply.github.com>
2020-07-24 00:25:57 +0530

    Initial commit

---
+++ assets/js/fullscreen.js
@@ -0,0 +1,39 @@
+let goFS = document.getElementById("goFS");
+var elem = document.documentElement;
+let fullscreen = false;
+
+goFS.addEventListener("click", function () {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+
+  /* Close fullscreen */
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  if (fullscreen) {
+    document.querySelector("#fullscreen_button").innerHTML = "Go Fullscreen";
+    fullscreen = false;
+  } else {
+    document.querySelector("#fullscreen_button").innerHTML = "Go Back";
+    fullscreen = true;
+  }
+});
